00b427e7f2b07a362d95ee6010d3c88c36a353b8
a <y>
2024-09-27 16:00:13 +0530

    friday m commit

--- serverside/server.js
+++ serverside/server.js
@@ -2,8 +2,19 @@ const http = require("http")
 const fs = require("fs")
 const url = require("url")
 port=4000
+const queryString=require("querystring")
+const {MongoClient}=require("mongodb")
+const { error } = require("console")
+//connect
+const client=new MongoClient('mongodb://127.0.0.1:27017/')
 
 const app = http.createServer((req,res)=>{
+    //DATABASE
+    const db=client.db("DONOR")
+
+    //COLLECTION
+    const collection=db.collection('bloodbank')
+
     const path=url.parse(req.url)
     console.log(path);
 
@@ -30,6 +41,28 @@ const app = http.createServer((req,res)=>{
     else if(path.pathname=='/js/donor.js'){
         res.writeHead(200,{"Content-Type":"text/js"})
         res.end(fs.readFileSync("../clientside/js/donor.js"))
+    }
+    else if(path.pathname=='/submit'&&req.method=="POST"){
+        console.log("hai");
+        let body=''
+        req.on("data",(chunks)=>{
+            console.log(chunks);
+            body+=chunks.toString()
+            console.log(body);
+        })
+        req.on("end",async()=>{
+            if(body!=null){
+                const formData=queryString.parse(body)
+                console.log(formData);
+                collection.insertOne(formData).then(()=>{
+                    console.log("data added");
+                }).catch((error)=>{
+                    console.log(error);
+                })
+                res.writeHead(200,{"Content-Type":"text/html"})
+                res.end(fs.readFileSync("../clientside/index.html"))
+            }
+        })
     }
 })
 app.listen(port)
